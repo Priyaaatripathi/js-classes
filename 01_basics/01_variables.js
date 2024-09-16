07a8b3b91f524ea5b66ac8c34d3cda5cf488a5b9
Priya tripathi <142152988+Priyaaatripathi@users.noreply.github.com>
2024-09-16 19:43:51 +0000

    datatypes_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=144352
+let accountEmail="pro@gmail.com"
+var accountPassword="jaipur"
+accountcity="gkp"
+let accountState;
+
+accountEmail="hc@123"
+accountPassword="21233"
+accountcity="Bengal"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState])
+
+// prefer  not to use var
+// becoz of issues of block scope and functionaal scopescope 
+// {
+// } this curly braces is known as scope
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3)  // we are using nodejs , not browser
+
+console.log(3+3);// code readability should be high
+
+console.log("priya");
+
+let name="priya";
+let age=19;
+let  isLoggedin=false;
+
+
+// PREMITIVE DATA TYPES
+// number => 2 to power 53
+// bigint
+// string =>" "
+// boolean => true/false
+// null=> standalone value
+// undefined => when value is not given or assigned
+// symbol=> use for uniqueness 
+
+// object
+
+console.log(typeof "priy");
+
+console.log(typeof undefined);  //undefined
+
+console.log(typeof age);// number
+console.log(typeof null);// object
+
