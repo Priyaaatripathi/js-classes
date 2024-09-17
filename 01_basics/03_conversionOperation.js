db3d08e98f63288e4fdf7d3f902f86180f694ac0
Priya tripathi <142152988+Priyaaatripathi@users.noreply.github.com>
2024-09-17 11:15:11 +0000

    conversionofoperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+let score="33abc";
+ 
+console.log(typeof score);
+//  or
+console.log(typeof(score));
+
+let a=33;
+console.log(typeof a);
+//  or
+console.log(typeof(a));
+
+let valueinnumber=Number(score);
+console.log(typeof valueinnumber );
+
+console.log(valueinnumber );
+
+let b=null;
+console.log(typeof b);
+
+// "33" => 33
+// "33abc" =>NaN i.e not a no.
+
+// true => 1;
+// false=> 0
+
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let isLoggedIn1="";
+let booleanIsLoggedIn1=Boolean(isLoggedIn1);
+console.log(booleanIsLoggedIn1);
+
+
+//  1=>true
+//  ""=> false
+//  " fhg"=> true
+
+let someno=33;
+
+let stringno=String(someno);
+
+console.log( typeof stringno);
+
