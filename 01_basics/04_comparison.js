7a44e31393721047de24325a7d06851797920410
Priya tripathi <142152988+Priyaaatripathi@users.noreply.github.com>
2024-09-20 11:44:21 +0000

    comparsion

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,30 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(1==2);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+//=== is used to check whether this is same datatype or not
+ console.log("2"===2);
+ console.log("a"==="a");
+ 
+
+
+
+
+
+
+
+
+
+
+
