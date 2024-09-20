55c3e0b8108ede0fdfd2c5320010823ea57cb9cd
Priya tripathi <142152988+Priyaaatripathi@users.noreply.github.com>
2024-09-20 12:35:00 +0000

    datatypesummary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,26 @@
+//Primitive dataTypes
+// 7 Types: String,Number,Boolean,null (means empty),undefined(when we don't declare values),Symbol,BigInt
+
+const bignumber=13234659087666555779;
+
+
+
+// Reference datatypes(Non primitive  datatype)
+//Array,Objects,Functions 
+
+const heros=["shaktiman","naagraj","doga"];
+let myObj={
+    name:"priya",
+    age:22,
+}
+
+const myFunction=function(){
+    console.log("Hello world");
+}
+console.log(typeof bignumber);
+
+console.log(typeof myFunction);
+
+
+//study sites ECMAScript
+//https://262.ecma-international.org/5.1/#sec-11.4.3
